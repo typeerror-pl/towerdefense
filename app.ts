4324e0b28fdf2e85510c7c7ebe7b9950fa337487
Kamil Rams <kamil.r@interia.pl>
2019-07-08 16:38:44 +0200

    drawing on screen - three colored rectangles

--- app.ts
+++ app.ts
@@ -1,2 +1,27 @@
-document.write('Hello world');
+// przygotowanie płótna do rysowania oraz pobranie jego kontekstu
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// rysowanie po kontekście płótna
+function draw(ctx: CanvasRenderingContext2D): void {
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(100, 100, 100, 100);
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(300, 100, 100, 100);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(500, 100, 100, 100);
+
+}
+
+if (context !== null) {
+    draw(context);
+}
+
+// dodanie płótna do elementu body
+document.body.appendChild(canvas);
